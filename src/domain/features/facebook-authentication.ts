b344c3237c9202a08b8ba54b7ab2a8c9a161ca33
Bruno Vallim <brunogvallim@gmail.com>
2022-05-30 09:52:22 -0300

    ensure Service calls LoadFacebookUserApi with correct params

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -4,7 +4,7 @@ import { AccessToken } from "@/domain/models"
 export interface FacebookAuthentication {
     perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
 }
-namespace FacebookAuthentication {
+export namespace FacebookAuthentication {
     export type Params = {
         token: string
     }
