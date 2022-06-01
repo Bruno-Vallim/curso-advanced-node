df66ab297cc7bdbcbaa5b64ccb82be70ef2fc2df
Bruno-Galhardi <brunogvallim@gmail.com>
2022-06-01 15:27:53 -0300

    ensure JwtTokeGenerator returns token on success

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -1,30 +1,45 @@
-import { TokenGenerator } from '@/data/contracts/crypto'
-import jwt from 'jsonwebtoken'
+import { TokenGenerator } from "@/data/contracts/crypto"
+import jwt from "jsonwebtoken"
 
-jest.mock('jsonwebtoken')
+jest.mock("jsonwebtoken")
 
 class JwtTokenGenerator {
-    constructor (private readonly secret: string) {}
+constructor(private readonly secret: string) {}
 
-    async generateToken (params: TokenGenerator.Params): Promise<void> {
-        const expirationInSeconds = params.expirationInMs / 1000
-        jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
-    }
+async generateToken(params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
+    const expirationInSeconds = params.expirationInMs / 1000
+    return jwt.sign({ key: params.key }, this.secret, {
+    expiresIn: expirationInSeconds
+    })
+}
 }
 
-describe('JwtTokenGenerator', () => {
-    let sut: JwtTokenGenerator
-    let fakeJwt: jest.Mocked<typeof jwt >
+describe("JwtTokenGenerator", () => {
+let sut: JwtTokenGenerator
+let fakeJwt: jest.Mocked<typeof jwt>
 
-    beforeAll(() => {
-    fakeJwt = jwt as jest.Mocked<typeof jwt >
-    })
-    beforeEach(() => {
-    sut = new JwtTokenGenerator('any_secret')
-    })
-it('should call sign with correct params', async () => {
-await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+    fakeJwt.sign.mockImplementation(() => 'any_token')
+})
+beforeEach(() => {
+    sut = new JwtTokenGenerator("any_secret")
+})
+it("should call sign with correct params", async () => {
+    await sut.generateToken({ key: "any_key", expirationInMs: 1000 })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+    expect(fakeJwt.sign).toHaveBeenCalledWith(
+    { key: "any_key" },
+    "any_secret",
+    { expiresIn: 1 }
+    )
+})
+it("should return a token", async () => {
+    const token = await sut.generateToken({
+    key: "any_key",
+    expirationInMs: 1000
+    })
+
+    expect(token).toEqual('any_token')
 })
 })
