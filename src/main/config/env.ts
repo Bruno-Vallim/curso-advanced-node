4ada7ec1828aa317c291b28d91ed83e82760995a
Bruno Vallim <brunogvallim@gmail.com>
2022-06-07 16:48:15 -0300

    add facebook Login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
         clientId: process.env.FB_CLIENT_ID ?? '1214721959374643',
         clientSecret: process.env.FB_CLIENT_SECRET ?? '6afa0060064f96f1727da79403f5f017'
     },
-    port: process.env.PORT ?? 8080
+    port: process.env.PORT ?? 8080,
+    jwtSecret: process.env.JWT_SECRET ?? '86mav3r1cK2022'
 }
