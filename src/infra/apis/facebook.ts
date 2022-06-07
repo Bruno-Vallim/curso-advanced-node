d51a2afd744f7d34621dea70b6b0d358d2940319
Bruno Vallim <brunogvallim@gmail.com>
2022-06-07 12:13:49 -0300

    change async await if favor of promise

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,5 +1,6 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis"
 import { HttpGetClient } from "@/infra/http"
+import { userInfo } from "os"
 
 type AppToken = {
     access_token: string
@@ -23,16 +24,9 @@ export class FacebookApi implements LoadFacebookUserApi {
         private readonly clientSecret: string
     ) { }
     async loadUser(params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-        try {
-            const userInfo = await this.getUserInfo(params.token)
-            return {
-                facebookId: userInfo.id,
-                name: userInfo.name,
-                email: userInfo.email
-            }
-        } catch {
-            return undefined
-        }
+        return this.getUserInfo(params.token)
+        .then(({ id, name, email }) => ({ facebookId: id, name, email }))
+        .catch(() => undefined)
     }
 
     private async getAppToken(): Promise<AppToken> {
