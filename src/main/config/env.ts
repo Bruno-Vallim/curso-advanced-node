24988ca606fdc8bf78e344c8b02d5a2cb606f142
Bruno Vallim <brunogvallim@gmail.com>
2022-06-07 15:43:20 -0300

    configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
     facebookApi: {
         clientId: process.env.FB_CLIENT_ID ?? '1214721959374643',
         clientSecret: process.env.FB_CLIENT_SECRET ?? '6afa0060064f96f1727da79403f5f017'
-    }
+    },
+    port: process.env.PORT ?? 8080
 }
