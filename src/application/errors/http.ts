55aa619aad8182b69b88fbfd84d060adb9f0fac7
Bruno Vallim <brunogvallim@gmail.com>
2022-06-28 13:19:42 -0300

    ensure AuthenticationMiddleware returns 403 if authorization is falsy

--- src/application/errors/http.ts
+++ src/application/errors/http.ts
@@ -17,3 +17,10 @@ export class UnauthorizedError extends Error {
     this.name = 'UnauthorizedError'
     }
 }
+
+export class ForbiddenError extends Error {
+    constructor() {
+    super('Access denied')
+    this.name = 'ForbiddenError'
+    }
+}
