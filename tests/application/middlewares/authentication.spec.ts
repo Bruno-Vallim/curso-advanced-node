14c065757f5aed83b149030e0705c9bb4beb1471
Bruno Vallim <brunogvallim@gmail.com>
2022-06-28 14:14:13 -0300

    move AuthenticationMiddleware to its won file

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,28 +1,5 @@
 import { ForbiddenError } from "@/application/errors/http"
-import { forbidden, HttpResponse, ok } from "@/application/helpers"
-import { RequiredStringValidator } from "@/application/validation"
-import { Authorize } from "@/domain/use-cases"
-
-type HttpRequest = { authorization: string }
-type Model = Error | { userId: string }
-
-class AuthenticationMiddleware {
-    constructor(private readonly authorize: Authorize) {}
-    async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        if (!this.validate({ authorization })) return forbidden()
-        try {
-        const userId = await this.authorize({ token: authorization })
-        return ok({ userId })
-        } catch {
-            return forbidden()
-        }
-    }
-    private validate({ authorization }: HttpRequest): Boolean {
-        const error = new RequiredStringValidator(authorization, 'authorization').validate()
-        return error === undefined
-    }
-}
+import { AuthenticationMiddleware } from "@/application/middlewares"
 
 describe('AuthenticationMiddleware', () => {
     let sut: AuthenticationMiddleware
