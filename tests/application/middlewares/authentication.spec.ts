1384606aeb26181f4013e0efca0cf89541efa1be
Bruno Vallim <brunogvallim@gmail.com>
2022-06-28 13:37:24 -0300

    ensure AuthenticationMiddleware calls authorize with correct input

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,18 +1,31 @@
 import { ForbiddenError } from "@/application/errors/http"
 import { forbidden, HttpResponse } from "@/application/helpers"
+import { RequiredStringValidator } from "@/application/validation"
+import { Authorize } from "@/domain/use-cases"
 
 type HttpRequest = { authorization: string }
 
 class AuthenticationMiddleware {
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-        return forbidden()
+    constructor(private readonly authorize: Authorize) {}
+    async handle({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+        const error = new RequiredStringValidator(authorization, 'authorization').validate()
+        if (error !== undefined) return forbidden()
+        await this.authorize({ token: authorization })
     }
 }
 
 describe('AuthenticationMiddleware', () => {
     let sut: AuthenticationMiddleware
+    let authorize: jest.Mock
+    let authorization: string
+
+    beforeAll(() => {
+        authorization = 'any_authorization_token'
+        authorize = jest.fn()
+    })
+
     beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(authorize)
     })
 
     it('should return 403 if authorization is empty', async () => {
@@ -34,13 +47,18 @@ describe('AuthenticationMiddleware', () => {
     })
 
     it('should return 403 if authorization is undefined', async () => {
-        const sut = new AuthenticationMiddleware()
-
         const httpResponse = await sut.handle({ authorization: undefined as any })
 
         expect(httpResponse).toEqual({
             statusCode: 403,
             data: new ForbiddenError()
         })
+    })
+
+    it('should call authorize with correct input', async () => {
+        await sut.handle({ authorization })
+
+        expect(authorize).toHaveBeenCalledWith({ token: authorization })
+        expect(authorize).toHaveBeenCalledTimes(1)
     })
 })
